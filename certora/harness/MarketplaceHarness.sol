ae995287919c3926712438d3a4b73ac4790fa4f1
0xb337r007 <0xe4e5@proton.me>
2024-06-06 16:12:13 +0200

    update harness

--- certora/harness/MarketplaceHarness.sol
+++ certora/harness/MarketplaceHarness.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.23;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Marketplace} from "../../contracts/Marketplace.sol";
 import {MarketplaceConfig} from "../../contracts/Configuration.sol";
+import {IGroth16Verifier} from "../../contracts/Groth16.sol";
 
 contract MarketplaceHarness is Marketplace {
-    constructor(IERC20 token_, MarketplaceConfig memory configuration) Marketplace(token_, configuration) {}
+    constructor(MarketplaceConfig memory configuration, IERC20 token_, IGroth16Verifier verifier)
+        Marketplace(configuration, token_, verifier)
+    {}
 
     function totalReceived() public view returns (uint256) {
         return _marketplaceTotals.received;
